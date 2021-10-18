0f927b169b0d28d2b22c226a8c90e0ce80622b0e
Demchenko Kirill <thegalaxydias@gmail.com>
2021-10-18 17:28:15 +0300

    Добавьте файлы проекта.

---
+++ Selenium.JavaScriptExecutor/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium.JavaScriptExecutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://itechcraft.com/");
+            driver.Manage().Window.Maximize();
+
+            
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+            IWebElement footer = driver.FindElement(By.Id("section__footer"));
+
+            js.ExecuteScript("arguments[0].scrollIntoView(); ", footer);
+           
+           
+
+        }
+
+       
+    }
+}
